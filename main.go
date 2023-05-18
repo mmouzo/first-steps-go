b03a0dc7f789c881852f422730fe7c72a0b57b90
mmouzo <mouzosilvam@gmail.com>
2023-05-18 19:21:46 +0200

    Changes to be committed: 	new file:   .gitignore 	new file:   go.mod 	new file:   go.sum 	new file:   main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+
+}
+
+func main() {
+
+	e := echo.New()
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "PONG!")
+	})
+
+	e.GET("user/:id", getUser)
+
+	e.Logger.Fatal(e.Start(":9999"))
+}
